bddbd009789d9b175d28be32b3165d5ab3e4bd28
gsaxena <gaurav.saxena@rsystems.com>
2021-03-02 16:40:22 +0530

    removed unnecessary code in in gitrepositorymanager.

--- Business.GitRepository/Managers/GitRepositoryManager.cs
+++ Business.GitRepository/Managers/GitRepositoryManager.cs
@@ -31,7 +31,6 @@
         private const string GitFolder = ".git";
         private const string TextMimeType = "text/plain";
         private Repository _repository;
-        private UsernamePasswordCredentials _userNamePasswordCredentials;
         private readonly object _syncRoot = new object();
         #endregion
 
@@ -47,12 +46,6 @@
             EnsureArg.IsNotEmptyOrWhiteSpace(gitConnection.GitRemoteLocation);
 
             _gitConnection = gitConnection;
-
-            _userNamePasswordCredentials = new UsernamePasswordCredentials()
-            {
-                Username = gitConnection.UserName,
-                Password = gitConnection.Password
-            };
         }
 
         /// <summary>
@@ -306,7 +299,6 @@
             cloneOptions.CertificateCheck += (certificate, valid, host) => true;
 
             cloneOptions.CredentialsProvider = (_url, _user, _cred) => new DefaultCredentials();
-            cloneOptions.CredentialsProvider += (_url, _user, _cred) => _userNamePasswordCredentials;
             Repository.Clone(_gitConnection.GitRemoteLocation, _gitConnection.GitLocalFolder,
             cloneOptions);
 
@@ -337,8 +329,6 @@
             var refSpecs = new List<string>() { network.FetchRefSpecs.First().Specification };
             var fetchOptions = new FetchOptions { TagFetchMode = TagFetchMode.All };
             fetchOptions.CredentialsProvider = (_url, _user, _cred) => new DefaultCredentials();
-            fetchOptions.CredentialsProvider += (_url, _user, _cred) => _userNamePasswordCredentials;
-
             _repository.Network.Fetch(network.Name, refSpecs, fetchOptions);
         }
 
